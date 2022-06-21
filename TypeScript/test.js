acb2432773f9760c7fa272354d8e25a0a673a43f
yiyu66 <1226572422@qq.com>
2022-06-21 23:34:21 +0800

    docs：ts 补充

--- TypeScript/test.js
+++ TypeScript/test.js
@@ -1,7 +1,19 @@
-function getValueByKey(p, key) {
-    return p[key];
+const obj = {
+    name: '张三',
+    getName() {
+        return this.name
+    },
+    getName1: () => {
+        return this.name
+    }
 }
-var val = getValueByKey({ name: "hello", age: 10, gender: "male" }, "name");
-console.log(val);
-
-// 修改测试
+obj.__proto__.getName2 = function() {
+    return this.name
+}
+obj.__proto__.getName3 = () => {
+    return this.name
+}
+console.log('普通函数',obj.getName())
+console.log('普通函数',obj.getName2())
+console.log('箭头函数',obj.getName1())
+console.log('箭头函数',obj.getName3())
--- TypeScript/test.ts
+++ TypeScript/test.ts
@@ -1,14 +1,15 @@
-
 interface Person {
-    name: string;
-    age: number;
-    gender: "male" | "female";
-  }
-  //type PersonKey = 'name'|'age'|'gender';
-  type PersonKey = keyof Person;
-  
-  function getValueByKey(p: Person, key: PersonKey) {
-    return p[key];
-  }
-  let val = getValueByKey({ name: "hello", age: 10, gender: "male" }, "name");
-  console.log(val);
+  name: string;
+  age: number;
+  gender: "male" | "female";
+}
+//type PersonKey = 'name'|'age'|'gender';
+type PersonKey = keyof Person;
+
+function getValueByKey(p: Person, key: PersonKey) {
+  return p[key];
+}
+let val = getValueByKey({ name: "hello", age: 10, gender: "male" }, "name");
+console.log(val);
+
+type ReturnType1<T> = T extends (...args: any[]) => infer R ? R : any;
